bf9a8bfc016c7ce099970761c345ee1dd389c261
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2025-12-12 10:29:24 +0000

    Task 2 complete

--- src/main/java/mercator/demo/TaskController.java
+++ src/main/java/mercator/demo/TaskController.java
@@ -1,8 +1,15 @@
 package mercator.demo;
 
-import org.apache.coyote.Response;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +23,36 @@ public class TaskController {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<Integer> qrCode() {
-        return new ResponseEntity<>(123, HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<BufferedImage> qrCode() {
+        BufferedImage image = getImage();
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(image);
     }
+
+
+    public BufferedImage getImage() {
+        int height = 250, width = 250;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = image.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        return image;
+    }
+
+
+
+
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> bufferedImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
+
+
+
 
     @GetMapping("/tasks")
     public List<Task> getTasks() {
