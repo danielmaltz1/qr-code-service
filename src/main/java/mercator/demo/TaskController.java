308640116085f8e6448557a50c01777ca161ba83
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2025-12-12 13:59:05 +0000

    Task 3 complete

--- src/main/java/mercator/demo/TaskController.java
+++ src/main/java/mercator/demo/TaskController.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import org.springframework.boot.jackson.autoconfigure.JacksonProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TaskController {
@@ -23,35 +25,51 @@ public class TaskController {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<BufferedImage> qrCode() {
-        BufferedImage image = getImage();
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_PNG)
-                .body(image);
+    public ResponseEntity<Object> qrCode(@RequestParam("size") String size, @RequestParam("type") String typeOfImage) {
+        int sizeInt = Integer.parseInt(size);
+        if (150 <= sizeInt && sizeInt <= 350) {
+            MediaType mediaType;
+            switch (typeOfImage) {
+                case "png":
+                    mediaType = MediaType.IMAGE_PNG;
+                    break;
+                case "jpeg":
+                    mediaType = MediaType.IMAGE_JPEG;
+                    break;
+                case "gif":
+                    mediaType = MediaType.IMAGE_GIF;
+                    break;
+                default:
+                    return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(
+                            Map.of("error", "Only png, jpeg and gif image types are supported")
+                    );
+            }
+            BufferedImage image = getImage(sizeInt);
+            return ResponseEntity
+                    .ok()
+                    .contentType(mediaType)
+                    .body(image);
+        } else {
+            return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(
+                    Map.of("error", "Image size must be between 150 and 350 pixels")
+            );
+        }
     }
 
 
-    public BufferedImage getImage() {
-        int height = 250, width = 250;
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    public BufferedImage getImage(int size) {
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
+        g.fillRect(0, 0, size, size);
         return image;
     }
-
-
-
 
 
     @Bean
     public HttpMessageConverter<BufferedImage> bufferedImageHttpMessageConverter() {
         return new BufferedImageHttpMessageConverter();
     }
-
-
-
 
 
     @GetMapping("/tasks")
